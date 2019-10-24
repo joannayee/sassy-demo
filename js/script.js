93ecc0a41c97b21b38346864bfa973a4b5b487e8
jyeeartist@gmail.com <nmastudent@KOAMAC014.local>
2019-10-24 12:37:24 -1000

    updated with improved overlay menu

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ const mymenubutton = document.querySelector('.menu-button');
 
 mymenubutton.onclick = function () {
     mysitenav.classList.toggle('active');
-} 
+}
+// Remove active class when user clicks on overlay nav links//
+const mysitenavlinks = document.querySelectorAll('.site-header .site-nav a');
+for (var i = 0, len = mysitenavlinks.length; i < len; i++) {
+    mysitenavlinks[i].onclick = function () {
+        mysitenav.classList.remove('active');
+    };
+};
